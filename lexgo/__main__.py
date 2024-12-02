48c4d97b0e6f185dcd21753e3211be0cdc1c07e3
Joshua Kilpatrick <kilpatrick.joshua@gmail.com>
2024-12-01 20:08:03 -0600

    adding first working version of the next lexgo tool. First version only supports english.

--- lexgo/__main__.py
+++ lexgo/__main__.py
@@ -1,4 +1,4 @@
-from .cli import cli
+from lexgo.cli import lexgo
 
 if __name__ == "__main__":
-    cli()
+    lexgo()
--- tests/test_lexgo.py
+++ tests/test_lexgo.py
@@ -1,10 +1,10 @@
 from click.testing import CliRunner
-from lexgo.cli import cli
+from lexgo.cli import lexgo
 
 
 def test_version():
     runner = CliRunner()
     with runner.isolated_filesystem():
-        result = runner.invoke(cli, ["--version"])
+        result = runner.invoke(lexgo, ["--version"])
         assert result.exit_code == 0
-        assert result.output.startswith("cli, version ")
+        assert result.output.startswith("lexgo, version ")
